275c8d4521ecf4f5eefa482a3f130359fdab9bf0
Bernhard Arnold <bernhard.arnold@cern.ch>
2021-05-26 15:50:12 +0200

    removed obsolete files

--- scripts/txt2json.sh
+++
@@ -1,9 +0,0 @@
-
-cd VPX33234
-
-for dir in `ls -d HPK_*`; do
-    for file in `ls -d $dir/*.txt`; do
-        echo $file
-        python ../../analysis-pqc/scripts/txt2json.py $file > $file.json
-    done
-done
