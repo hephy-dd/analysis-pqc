0d424cf9c52c8ace41f97674a0bd33d835bce502
Moritz Wiehe <m.wiehe@cern.ch>
2021-10-12 15:34:00 +0200

    smarter way to call ext scripts..

--- scripts/upload_db.py
+++ scripts/upload_db.py
@@ -9,10 +9,12 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(filename='xml_upload.log', level=logging.INFO)#, format='%(levelname)s:%(name)s:%(message)s')
 
+script_path = os.path.dirname(os.path.realpath(__file__))  # scripts directory
+
 
 def get_run_number(db_instance):
-    
-    p1 = subprocess.run( ['python3', 'scripts'+os.sep+'ext'+os.sep+'rhapi.py', '--login', '--url=https://cmsdca.cern.ch/trk_rhapi', '-f', 'csv', '--clean',f"select r.run_number from trker_{db_instance}.trk_ot_test_nextrun_v r"], capture_output=True)
+    ext_filename = os.path.join(script_path, 'ext', 'rhapi.py')    
+    p1 = subprocess.run( ['python3', ext_filename, '--login', '--url=https://cmsdca.cern.ch/trk_rhapi', '-f', 'csv', '--clean',f"select r.run_number from trker_{db_instance}.trk_ot_test_nextrun_v r"], capture_output=True)
     #python3 ext/rhapi.py --login --url=https://cmsdca.cern.ch/trk_rhapi -f csv --clean "select r.run_number from trker_cmsr.trk_ot_test_nextrun_v r"
     answer = p1.stdout.decode()
     answer = answer.split()
@@ -23,10 +25,9 @@ def get_run_number(db_instance):
 
 
 def upload_to_db(filename, db_instance):
-
+    ext_filename = os.path.join(script_path, 'ext', 'cmsdbldr_client.py')    
     try:
-        #p1 = subprocess.run(['python3', 'ext'+os.sep+'cmsdbldr_client.py', '--login', f'--url=https://cmsdca.cern.ch/trk_loader/trker/{db_instance}', f'{filename}'],  capture_output=True)
-        p1 = subprocess.run(['python3', 'scripts'+os.sep+'ext'+os.sep+'cmsdbldr_client.py', '--login', f'--url=https://cmsdca.cern.ch/trk_loader/trker/{db_instance}', f'{filename}'])
+        p1 = subprocess.run(['python3', ext_filename, '--login', f'--url=https://cmsdca.cern.ch/trk_loader/trker/{db_instance}', f'{filename}'])
         # python3 ext/cmsdbldr_client.py --login --url=https://cmsdca.cern.ch/trk_loader/trker/int2r filename
         answer = p1.stdout.decode()
         answer = answer.split()
@@ -68,11 +69,13 @@ def run(path,db):
     choice = input().lower()
     if choice == 'yes':
         for ifile,filename in enumerate(filenames):
-        
-            #run=get_run_number(db_instance) ##Query of run number not necessary, will be assigned on upload
+
+            ##Query of run number not necessary, will be assigned on upload
+            #run=get_run_number(db_instance) 
             #update_file(filename,'<RUN_NUMBER></RUN_NUMBER>',f'<RUN_NUMBER>{run}</RUN_NUMBER>')
 
             upload_to_db(filename,db_instance)
+            
     else:
         print('Nothing uploaded')    
 
