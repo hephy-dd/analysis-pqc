35f457c8dd9524098eb777475dcb6d2e5a92eacf
thisdani <daniela75.kugler@gmail.com>
2023-08-21 11:25:38 +0200

    new numerical gcd analysis

--- scripts/pqc_analysis_json.py
+++ scripts/pqc_analysis_json.py
@@ -500,7 +500,7 @@ def analyse_gcd_data(path, options=None, config=None):
     lbl = assign_label(path, test)
 
     gcd_result = analyse_gcd(v, i_em, maxreldev=0.03, **kwargs)
-    print("This is in the analysis: I_surf: {x}; I_bulk: {y}".format(x=gcd_result.i_surf, y=gcd_result.i_bulk))
+    
 
     if options.plot:
         fig, ax = plt.subplots(1, 1)
